5756d8e95bcca43e1c8319e673c7da3a79f7a6ca
NemalPerera-NP <nemalperera1999@gmail.com>
2024-03-29 17:28:52 +0530

    testing function for test input field editable option

--- src/components/screens/signupPage.js
+++ src/components/screens/signupPage.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "../styles/signup.module.css";
-import styleslogin from "../styles/login.module.css"
+import styleslogin from "../styles/login.module.css";
 
 function SignupPage() {
   const navigate = useNavigate();
@@ -15,89 +15,103 @@ function SignupPage() {
   const [nic, setNic] = useState("");
   const [employeeID, setEployeeID] = useState("");
   const [signupError, setSignuprror] = useState("");
+  const [isEditable, setIsEditable] = useState(false);
 
   const emailVal = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
   //navigate("/"); function to navigate to login page
 
-  const submitSignup = async () => {};
+  const submitSignup = async () => {
+    console.log("submit pressed");
+  };
+
+  ///testing function
+  const change = () => {
+    setIsEditable(true);
+  };
 
   return (
     <div className={styleslogin.login_container}>
       <div className={styles.signup_form_container}>
         <div className={styles.signup_form_container_sub}>
-        <form className={styles.form_container} onSubmit={submitSignup}>
-          <input
-            type="text"
-            id="Name"
-            placeholder="Name with Initials"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-            className={styles.input}
-          />
-          <input
-            type="text"
-            id="NIC"
-            placeholder="NIC"
-            value={nic}
-            onChange={(e) => setNic(e.target.value)}
-            required
-            className={styles.input}
-          />
-          <input
-            type="text"
-            id="username"
-            placeholder="UserName"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-            className={styles.input}
-          />
-          <input
-            type="email"
-            id="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className={styles.input}
-          />
-          <input
-            type="text"
-            id="employeeID"
-            placeholder="Employee ID"
-            value={employeeID}
-            onChange={(e) => setEployeeID(e.target.value)}
-            required
-            className={styles.input}
-          />
-          <input
-            type="password"
-            id="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className={styles.input}
-          />
-          <input
-            type="password"
-            id="confirmpassword"
-            placeholder="Confirm Password"
-            value={confirmpassword}
-            onChange={(e) => setConfirmpassword(e.target.value)}
-            required
-            className={styles.input}
-          />
+          <form className={styles.form_container} onSubmit={submitSignup}>
+            <input
+              type="text"
+              id="Name"
+              placeholder="Name with Initials"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              className={styles.input}
+            />
+            <input
+              type="text"
+              id="NIC"
+              placeholder="NIC"
+              value={nic}
+              onChange={(e) => setNic(e.target.value)}
+              required
+              readOnly={!isEditable}
+              className={styles.input}
+            />
+            <input
+              type="text"
+              id="username"
+              placeholder="UserName"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              className={styles.input}
+            />
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className={styles.input}
+            />
+            <input
+              type="text"
+              id="employeeID"
+              placeholder="Employee ID"
+              value={employeeID}
+              onChange={(e) => setEployeeID(e.target.value)}
+              required
+              className={styles.input}
+            />
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className={styles.input}
+            />
+            <input
+              type="password"
+              id="confirmpassword"
+              placeholder="Confirm Password"
+              value={confirmpassword}
+              onChange={(e) => setConfirmpassword(e.target.value)}
+              required
+              className={styles.input}
+            />
 
-          {signupError && <div className={styles.error_msg}>{signupError}</div>}
-          <button type="submit" className={styles.green_btn}>
-            Sing Up
-          </button>
-        </form>
+            {signupError && (
+              <div className={styles.error_msg}>{signupError}</div>
+            )}
+            <button type="submit" className={styles.green_btn}>
+              Sing Up
+            </button>
+            {/* this is a button to check the isEditable option in input fields so delete before final release */}
+            <button type="Change" className={styles.green_btn} onClick={change}>
+              Change Up
+            </button>
+          </form>
         </div>
-        
       </div>
     </div>
   );
