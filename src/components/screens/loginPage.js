18f80cc6857fbec0ee08d4362d823af6ee040f21
NemalPerera-NP <nemalperera1999@gmail.com>
2024-03-29 10:31:13 +0530

    added a background image to the login page

--- src/components/screens/loginPage.js
+++ src/components/screens/loginPage.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "../styles/login.module.css";
 
 //logo image should be png
 function LoginPage() {
-  return <div>Login Page</div>;
+  const navigate = useNavigate();
+
+  return (
+
+    <div className={styles.login_container}>Login Page
+    <div className={styles.login_form_container}>
+        <div className={styles.login_logo_container}></div>
+        <form className={styles.login_form_container}>
+
+        </form>
+
+
+    </div>
+
+  </div>
+  );
+  
 }
 
 export default LoginPage;
